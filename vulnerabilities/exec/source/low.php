955447c40e4733c82739d16db045626b6c04ee0b
M D Sathees Kumar <60444051+Monst3rSec@users.noreply.github.com>
2025-09-14 17:54:09 +0530

    Revert "Harden exec low level against command injection"

--- vulnerabilities/exec/source/low.php
+++ vulnerabilities/exec/source/low.php
@@ -1,42 +1,21 @@
 <?php
 
-// Hardened ping functionality to prevent OS command injection.
-if( isset( $_POST[ 'Submit' ] ) ) {
+if( isset( $_POST[ 'Submit' ]  ) ) {
 	// Get input
 	$target = $_REQUEST[ 'ip' ];
 
-	// Block shell metacharacters outright
-	if( preg_match( '/[;|&`$()<>\n\r]/', $target ) ) {
-		$html .= '<pre>Invalid input.</pre>';
-	}
-	// Allow only IPv4 addresses or hostnames (letters, digits, dots, hyphens)
-	elseif( ! ( filter_var( $target, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 ) ||
-		preg_match( '/^[A-Za-z0-9.-]+$/', $target ) ) ) {
-		$html .= '<pre>Invalid input.</pre>';
+	// Determine OS and execute the ping command.
+	if( stristr( php_uname( 's' ), 'Windows NT' ) ) {
+		// Windows
+		$cmd = shell_exec( 'ping  ' . $target );
 	}
 	else {
-		// Build safe command using fixed binary and escaped argument
-		$cmd = '/bin/ping -c 3 -W 2 ' . escapeshellarg( $target );
-		$output = shell_exec( $cmd );
-
-		if( $output !== null ) {
-			$lines = explode( "\n", $output );
-			$safe  = array();
-
-			// Return only typical ping result lines
-			foreach( $lines as $line ) {
-				if( preg_match( '/^[A-Za-z0-9\s\.:,()=%-]+$/', $line ) ) {
-					$safe[] = $line;
-				}
-			}
-
-			$html .= '<pre>' . implode( "\n", $safe ) . '</pre>';
-		}
-		else {
-			// Do not expose raw shell output on failure
-			$html .= '<pre>Ping failed.</pre>';
-		}
+		// *nix
+		$cmd = shell_exec( 'ping  -c 4 ' . $target );
 	}
+
+	// Feedback for the end user
+	$html .= "<pre>{$cmd}</pre>";
 }
 
 ?>
